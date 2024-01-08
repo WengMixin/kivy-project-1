61eafd0eb8c7d0115eb9a6a998e6b105ea1f9bd9
WengMixin <mixin_weng2022@163.com>
2024-01-08 17:48:09 +0800

    add usb

--- main.py
+++ main.py
@@ -1,18 +1,54 @@
+import threading
 from kivy.app import App
 from kivy.uix.button import Button
 from kivy.uix.floatlayout import FloatLayout
 from kivy.uix.label import Label
 from kivy.uix.textinput import TextInput
+from kivy.uix.spinner import Spinner
+from pyudev import Context, Monitor, MonitorObserver
 
 
 class Interface(FloatLayout):
+    def __init__(self, **kwargs):
+        super(Interface, self).__init__(**kwargs)
+        self.spinner = Spinner(
+            text='USB Devices',
+            values=('No USB Device Detected',),
+            size_hint=(None, None),
+            size=(100, 44),
+            pos_hint={'center_x': .5, 'center_y': .5})
+        self.add_widget(self.spinner)
+
+    def update_usb_devices(self, action, device):
+        if device.device_type == 'usb_device':
+            if action == 'add':
+                print("USB Device Connected.")
+                self.ids.label.text = "USB Device Detected."
+                # Update the spinner values
+                self.ids.usb_spinner.values = (device.device_node,)
+            elif action == 'remove':
+                print("USB Device Disconnected.")
+                self.ids.label.text = "USB Device Disconnected."
+                self.ids.usb_spinner.values = ('No USB Device Detected',)
+
     def display_information(self):
         data = self.ids.textInput.text
         self.ids.label.text = data
 
 
 class ProjectApp(App):
-    pass
+    def build(self):
+        interface = Interface()
+        # Set up USB monitoring in a separate thread
+        context = Context()
+        monitor = Monitor.from_netlink(context)
+        monitor.filter_by(subsystem='usb')
+        observer = MonitorObserver(
+            monitor, callback=interface.update_usb_devices)
+        usb_thread = threading.Thread(target=observer.start)
+        usb_thread.daemon = True
+        usb_thread.start()
+        return interface
 
 
 ProjectApp().run()
