67322bf47a5a59d55a27e503d254dc6c4dd7d232
mixin weng <119753827+WengMixin@users.noreply.github.com>
2024-01-09 14:11:31 +0800

    Update main.py

--- main.py
+++ main.py
@@ -31,6 +31,17 @@ class Interface(FloatLayout):
                 self.ids.label.text = "USB Device Disconnected."
                 self.ids.usb_spinner.values = ('No USB Device Detected',)
 
+     def display_error(self, error):
+        self.ids.label.text = "Error: " + str(error)
+
+    def safe_update_usb_devices(self, action, device):
+        try:
+            self.update_usb_devices(action, device)
+        except Exception as e:
+            error = traceback.format_exc()
+            print(error)  # 打印错误到控制台
+            self.display_error(error)  # 显示错误信息到 UI
+
     def display_information(self):
         data = self.ids.textInput.text
         self.ids.label.text = data
@@ -44,7 +55,7 @@ class ProjectApp(App):
         monitor = Monitor.from_netlink(context)
         monitor.filter_by(subsystem='usb')
         observer = MonitorObserver(
-            monitor, callback=interface.update_usb_devices)
+            monitor, callback=interface.safe_update_usb_devices)  # 使用包装后的方法
         usb_thread = threading.Thread(target=observer.start)
         usb_thread.daemon = True
         usb_thread.start()
