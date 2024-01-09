35d97507d030f1a9f804da963122873898dab98b
WengMixin <mixin_weng2022@163.com>
2024-01-09 15:21:28 +0800

    add try-expect

--- main.py
+++ main.py
@@ -1,9 +1,9 @@
-import threading
+import traceback
 from kivy.app import App
 from kivy.uix.floatlayout import FloatLayout
-from kivy.uix.spinner import Spinner
-from jnius import autoclass
+from jnius import autoclass, JavaException
 
+# Android 类的引用
 Context = autoclass('android.content.Context')
 UsbManager = autoclass('android.hardware.usb.UsbManager')
 PythonActivity = autoclass('org.kivy.android.PythonActivity')
@@ -12,21 +12,35 @@ PythonActivity = autoclass('org.kivy.android.PythonActivity')
 class Interface(FloatLayout):
     def __init__(self, **kwargs):
         super(Interface, self).__init__(**kwargs)
-        self.add_widget(self.spinner)
         self.usb_manager = None
         self.setup_usb_manager()
 
     def setup_usb_manager(self):
-        activity = PythonActivity.mActivity
-        self.usb_manager = activity.getSystemService(Context.USB_SERVICE)
+        try:
+            activity = PythonActivity.mActivity
+            self.usb_manager = activity.getSystemService(Context.USB_SERVICE)
+            self.update_usb_devices()  # 更新 USB 设备列表
+        except JavaException as je:
+            print("Java Exception: ", je)
+            # 在这里处理 Java 层面的异常
+        except Exception as e:
+            print("Exception: ", e)
+            # 在这里处理 Python 层面的异常
 
     def update_usb_devices(self):
-        device_list = self.usb_manager.getDeviceList().values()
-        device_names = [device.getDeviceName() for device in device_list]
-        if device_names:
-            self.ids.usb_spinner.values = device_names
-        else:
-            self.ids.usb_spinner.values = ('No USB Device Detected',)
+        try:
+            device_list = self.usb_manager.getDeviceList().values()
+            device_names = [device.getDeviceName() for device in device_list]
+            if device_names:
+                self.ids.usb_spinner.values = device_names
+            else:
+                self.ids.usb_spinner.values = ('No USB Device Detected',)
+        except JavaException as je:
+            print("Java Exception: ", je)
+            # 在这里处理 Java 层面的异常
+        except Exception as e:
+            print("Exception: ", e)
+            # 在这里处理 Python 层面的异常
 
 
 class ProjectApp(App):
